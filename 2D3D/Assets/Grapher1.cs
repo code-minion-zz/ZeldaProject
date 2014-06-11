5c1fb299c74761951d204bef195ae0987110f819
Kit <chanwk3@gmail.com>
2014-06-11 23:46:01 +1200

    doing experiments with particles, not relevant to other scenes

---
+++ 2D3D/Assets/Grapher1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grapher1 : MonoBehaviour {
+
+	public int resolution = 10;
+
+	private ParticleSystem.Particle[] points;
+
+	// Use this for initialization
+	void Start () 
+	{		
+		if (resolution < 10 || resolution > 100) {
+			Debug.LogWarning("Grapher resolution out of bounds, resetting to minimum.", this);
+			resolution = 10;
+		}
+		points = new ParticleSystem.Particle[resolution];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
